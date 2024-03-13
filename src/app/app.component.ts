7df7d52153d46d142a937189d91bef446ead8a44
Solomiia Sahala <solomiia.sahala@sigma.software>
2024-03-13 10:45:12 +0200

    added functionality to validate password/ changed css to scss.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,70 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormControl} from '@angular/forms';
+
+enum ValidationClass {
+  EMPTY = 'empty',
+  SHORT = 'short',
+  EASY = 'easy',
+  MEDIUM = 'medium',
+  STRONG = 'strong'
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'password-strength';
+export class AppComponent implements OnInit {
+  passwordForm: FormControl;
+  validationClass: ValidationClass;
+
+  readonly blockClasses = ['easy', 'medium', 'strong'];
+
+  ngOnInit(): void {
+    this.passwordForm = new FormControl('');
+
+    this.passwordForm?.valueChanges.subscribe((value) => {
+      this.validatePassword(value);
+    });
+  }
+
+  validatePassword(value: string): void {
+    if (!value) {
+      this.validationClass = ValidationClass.EMPTY;
+    } else if (value.length < 8) {
+      this.validationClass = ValidationClass.SHORT;
+    } else if (this.containsOnlyDigits(value) || this.containsOnlyLetters(value) || this.containsOnlySymbols(value)) {
+      this.validationClass = ValidationClass.EASY;
+    } else if (this.containsLettersSymbolsAndNumbers(value)) {
+      this.validationClass = ValidationClass.STRONG;
+    } else {
+      // Combination of letters-symbols/letters-digits/digits-symbols
+      this.validationClass = ValidationClass.MEDIUM;
+    }
+  }
+
+  private containsOnlyDigits(value): boolean {
+    // Regular expression to match only digits
+    const regex = /^\d+$/;
+    return regex.test(value);
+  }
+
+  private containsOnlyLetters(value): boolean {
+    // Regular expression to match only letters (case-insensitive)
+    const regex = /^[a-zA-Z]+$/;
+    return regex.test(value);
+  }
+
+  private containsOnlySymbols(value): boolean {
+    // Regular expression to match only symbols (non-alphanumeric characters)
+    const regex = /^[^\w\s]+$/;
+    return regex.test(value);
+  }
+
+  private containsLettersSymbolsAndNumbers(value): boolean {
+    // Regular expression to match at least one letter, one symbol, and one number
+    const regex = /(?=.*[a-zA-Z])(?=.*[^\w\s])(?=.*\d)/;
+    return regex.test(value);
+  }
 }
+
